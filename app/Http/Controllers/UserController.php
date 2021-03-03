ca6ea70a1b48ddf858aef0b07c5997e75e742baa
dossmedaziz <dossaziz4@gmail.com>
2021-03-03 11:44:57 +0100

    auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth ; 
+use Illuminate\Support\Facades\Validator ;
 
 class UserController extends Controller
 {
@@ -73,4 +75,27 @@ class UserController extends Controller
                 return response()->json(['message'=>'Deleted']) ;
 
             }
+
+
+
+
+            public function login(Request $request)
+            {
+                $login = $request->validate([
+                    'email' => 'required|string',
+                    'password' => 'required|string'
+                ]) ;
+    
+             dd($login) ; 
+                    
+                        if(!Auth::attempt($login))
+                        {
+                            return response(['message'=>'invalid login credentials','access'=>'0']);
+                        }
+                        
+                        $accessToken = Auth::user()->createToken('authToken')->accessToken ; 
+    
+                        return response(['user'=>Auth::user(), 'access_token' => $accessToken ,'access' =>'1']) ;
+    
+                    }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\models\User ; 
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        User::create([
+                      'name'=>'admin',
+                      'email'=>'admin@admin.com',
+                      'password'=>'nachd-it',
+                      'phone_number'=>'12345678',
+                    //   'role_id'=>'1',
+                      'verified'=>'1',
+                      ]);
+    
+    }
+}
